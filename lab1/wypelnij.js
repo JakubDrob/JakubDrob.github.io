2d144a695eb6b85cbd835ab47fa4350dc88adfa4
kublot <kuba.drobinski@wp.pl>
2022-10-08 20:22:22 +0200

    finalpush

---
+++ lab1/wypelnij.js
@@ -0,0 +1,13 @@
+function Wypelnij(){
+	document.getElementById("email").value = "jakub.drobinski@gmail.com";
+	document.getElementById("kod_pocztowy").value = "94-122";
+	document.getElementById("numer_nip").value = "345-253-55-34";
+	document.getElementById("numer_dowodu").value = "ABC123456";
+	document.getElementById("adres_ipv4").value = "192.0.2.146";
+	document.getElementById("strona_www").value = "www.wikamp.pl";
+	document.getElementById("ścieżka_litera_dysku_małe").value = "C:\\windows\\temp";
+	document.getElementById("ścieżka_litera_dysku").value = "C:\\windows\\Temp";
+	document.getElementById("ścieżka_do_pliku_w_katalogu_etc").value = "/etc/passwd";
+	document.getElementById("adres_ipv6").value = "FE80:0000:0000:0000:0202:B3FF:FE1E:8329";
+	document.getElementById("numer_telefonu").value = "+48675987777";
+}
