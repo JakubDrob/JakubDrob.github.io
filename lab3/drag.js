1e0a078f80d9cd2e9f7e470dcc3849f9fcc1a3eb
kublot <kuba.drobinski@wp.pl>
2022-11-05 18:50:13 +0100

    color

--- lab3/drag.js
+++ lab3/drag.js
@@ -25,6 +25,20 @@ let color;
 	 }
     }
    }
+   
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
+ function setRandomColor() {
+	color = getRandomColor();
+	document.getElementById('box').style.background = color;
+  }
 
   document.addEventListener("dragstart", ({target}) => {
       if (target.className == "dropelement") {
